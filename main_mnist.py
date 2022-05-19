4c433cb4bfdf426fdc6fa927e434b61211171a93
lorenzo <lollopes1995@gmail.com>
2022-05-19 15:39:23 +0200

    binarized the input dataset and added tests for verifying that weights are binary. this could already be something implementable

--- main_mnist.py
+++ main_mnist.py
@@ -56,17 +56,17 @@ test_loader = torch.utils.data.DataLoader(
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.infl_ratio=3
-        self.fc1 = BinarizeLinear(784, 2048*self.infl_ratio)
+        self.infl_ratio=1
+        self.fc1 = BinarizeLinear(784, 100*self.infl_ratio)
         self.htanh1 = nn.Hardtanh()
-#        self.bn1 = nn.BatchNorm1d(2048*self.infl_ratio)
-#        self.fc2 = BinarizeLinear(2048*self.infl_ratio, 2048*self.infl_ratio)
-#        self.htanh2 = nn.Hardtanh()
-#        self.bn2 = nn.BatchNorm1d(2048*self.infl_ratio)
-#        self.fc3 = BinarizeLinear(2048*self.infl_ratio, 2048*self.infl_ratio)
-#        self.htanh3 = nn.Hardtanh()
-        self.bn3 = nn.BatchNorm1d(2048*self.infl_ratio)
-        self.fc4 = nn.Linear(2048*self.infl_ratio, 10)
+        self.bn1 = nn.BatchNorm1d(100*self.infl_ratio)
+        self.fc2 = BinarizeLinear(100*self.infl_ratio, 100*self.infl_ratio)
+        self.htanh2 = nn.Hardtanh()
+        self.bn2 = nn.BatchNorm1d(100*self.infl_ratio)
+        self.fc3 = BinarizeLinear(100*self.infl_ratio, 100*self.infl_ratio)
+        self.htanh3 = nn.Hardtanh()
+        self.bn3 = nn.BatchNorm1d(100*self.infl_ratio)
+        self.fc4 = nn.Linear(100*self.infl_ratio, 10)
         self.logsoftmax=nn.LogSoftmax()
         self.drop=nn.Dropout(0.5)
 
