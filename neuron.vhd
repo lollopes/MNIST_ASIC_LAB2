17cae83de729807be4bac0c6a7e71883b2134f06
Karan Pathak <89991968+karnapathak@users.noreply.github.com>
2022-05-23 03:59:41 +0530

    Add files via upload
    binary neuron model with signum activation function

---
+++ neuron.vhd
@@ -0,0 +1,97 @@
+--------------------------------------------------------------------------------
+--! @file
+--! @brief Single neuron instance
+--! @author Karan Pathak
+--------------------------------------------------------------------------------
+
+--! Use standard library with logic elements
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+ use IEEE.numeric_std.all;
+--! A single neuron with generic "N" inputs
+
+entity neuron is
+	generic(
+		inputs : integer := 3            --! Number of inputs into the neuron
+	);
+	port(
+		clk      : in  std_logic;        --! Clock input
+		rst      : in  std_logic;        --! Reset input
+		start_i  : in  std_logic;        --! Start input, indicates to start
+		                                 --! the calculation
+		input_i  : in  std_logic_vector(inputs - 1 downto 0); --! Neuron inputs
+		weight_i : in  std_logic_vector(inputs -1 downto 0); --! Neuron weights 
+		                                                  
+		output_o : out std_logic;   --! Neuron output
+		done_o   : out std_logic  := '0' --! Done output, indicates completion
+	);
+end entity neuron;
+
+
+architecture behavioral of neuron is
+    signal index : integer := inputs;
+	type state is (idle, start, acum, act_func);
+	signal current_state, next_state : state;
+	signal input_s, weight_s : std_logic_vector(inputs - 1 downto 0);
+	signal done_s  : std_logic ; 
+	signal mul_value : std_logic_vector(inputs-1 downto 0); -- change this everytime you change the number of inputs     
+	--signal popcount :std_logic_vector (inputs-1 downto 0) := (others => '0');
+	 
+begin
+
+	fsm_lower : process(clk, rst) is
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				current_state <= idle;
+			else
+			current_state <= next_state;
+			end if;
+		end if;
+	end process fsm_lower;
+
+	fsm_upper : process(current_state, start_i,index) is
+		variable popcount : std_logic_vector ( (inputs-1) downto 0) := (others => '0');
+	begin
+		case current_state is
+			when idle =>
+				done_s <= '0';
+				if start_i = '1' then
+					next_state <= start;
+				else
+					next_state <= current_state;
+				end if;
+
+			when start =>
+				for i in 0 to (inputs-1) loop
+                            mul_value(i) <= std_logic ( input_s(i) xnor weight_s(i));
+                end loop;
+                
+				next_state <= acum;
+
+			when acum =>
+				
+				    for j in 0 to (inputs-1) loop 
+					popcount := popcount + mul_value(j);
+					--index <= index -1;
+				    end loop;
+				    
+				    next_state <= act_func;
+
+			when act_func =>
+				done_s     <= '1';
+				if popcount > (inputs-1)/2 then
+				    
+			        output_o <= '1';
+			    else 
+			        output_o <= '0';
+				    next_state <= idle;
+                end if;
+		end case;
+
+	end process fsm_upper;
+    input_s <= input_i;
+    weight_s <= weight_i;  
+    done_o <= done_s;
+end architecture behavioral;
---
+++ neuron_tb.vhd
@@ -0,0 +1,90 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity neuron_tb is
+end entity neuron_tb;
+
+architecture tb of neuron_tb is
+
+component neuron is
+	generic(
+		inputs : integer := 3            --! Number of inputs into the neuron
+	);
+	port(
+		clk      : in  std_logic;        --! Clock input
+		rst      : in  std_logic;        --! Reset input
+		start_i  : in  std_logic;        --! Start input, indicates to start
+		                                 --! the calculation
+		input_i  : in  std_logic_vector(inputs - 1 downto 0); --! Neuron inputs
+		weight_i : in  std_logic_vector(inputs -1 downto 0); --! Neuron weights 
+		                                                  
+		output_o : out std_logic;   --! Neuron output
+		done_o   : out std_logic  := '0' --! Done output, indicates completion
+	);
+end component;
+
+	constant half_period : time := 10 ns;
+
+	constant inputs : integer := 3;
+
+	signal clk      : std_logic                         := '0';
+	signal rst      : std_logic                         := '0';
+	signal start_i  : std_logic                         := '0';
+	signal input_i  : std_logic_vector(inputs - 1 downto 0);
+	signal weight_i : std_logic_vector(inputs-1 downto 0);
+	signal output_o : std_logic;
+	signal done_o   : std_logic;
+
+begin
+
+	UUT : neuron 
+		generic map(
+			inputs => inputs
+		)
+		port map(
+			clk      => clk,
+			rst      => rst,
+			start_i  => start_i,
+			input_i  => input_i,
+			weight_i => weight_i,
+			output_o => output_o,
+			done_o   => done_o
+		); 
+	clock_process:	process 
+    
+        begin
+        for i in 0 to 50 loop
+        clk <= '0';
+        wait for 10 ns;
+        clk <= '1';
+        wait for 10 ns;
+        end loop;
+        wait;
+       --clk <= not clk after half_period;
+	   end process;
+	reset_process : process
+	   begin
+	      rst <= '1';
+	      wait for 10 ns;
+	      rst <='0';
+	      wait for 100ns;
+	      wait;
+	   end process;
+    stimuli: process 
+
+        begin		
+--        wait for 20 ns;
+--        input_i <= "101";
+--        weight_i <= "001";
+--        start_i<='1';
+        wait for 20 ns;
+        input_i <= "000";
+        weight_i <= "111";
+        start_i<='1';
+        
+            wait;
+--           start_i <= '0', '1' after 15 ns, '0' after 30 ns;
+     end process;         
+end architecture tb;
