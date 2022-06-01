6b95eef1e59bed216a4ff0689efabc87060dbf06
jasco <jccx19@gmail..com>
2022-06-01 19:03:08 +0200

    csv reading functions and process implemented (not tested)

--- RTL_Design/TB/layer_tb.vhd
+++ RTL_Design/TB/layer_tb.vhd
@@ -1,15 +1,18 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
---use work.types.all;
+use std.textio.all;
 use work.weight_matrix_pkg.all;
+
 entity layer_tb is
+    --empty
 end entity layer_tb;
 
 architecture behavioural of layer_tb is
 	
 	constant inputs : natural := 5;
 	constant outputs : natural := 4;
+	constant LINE_LENGTH_MAX: integer := 256;
 	
 	constant half_period : time := 10 ns;
 	
@@ -21,6 +24,39 @@ architecture behavioural of layer_tb is
 	signal done_o   : std_logic;
 	signal output_o :std_logic_vector (outputs-1 downto 0);
 	
+	--signals for reading the file
+	file my_csv_file: text;                         -- file
+    shared variable current_line: line;	            -- cache one line at a time for read operations
+    
+	--functions to read the file
+	function read_string return string is
+            variable return_string: string(1 to LINE_LENGTH_MAX);
+            variable read_char: character;
+            variable read_ok: boolean := true;
+            variable index: integer := 1;
+        begin
+            read(current_line, read_char, read_ok);
+            while read_ok loop
+                if read_char = ',' then
+                    return return_string;
+                else
+                    return_string(index) := read_char;
+                    index := index + 1;
+                end if;
+                read(current_line, read_char, read_ok);
+            end loop;
+	end function;  
+	
+	impure function read_integer return integer is
+            variable read_value: integer;
+            variable dummy_string: string(1 to LINE_LENGTH_MAX);
+        begin
+            read(current_line, read_value);
+            dummy_string := read_string;
+            return read_value;
+    end;
+	
+	--dut
 	component layer is
 	generic(
 		inputs  : natural := 5;         --! Network inputs = number of neurons in the previous layer
@@ -39,12 +75,9 @@ architecture behavioural of layer_tb is
 end component;
 
 begin
-
+    --DUT instatiation
     layer_inst : layer
-		generic map(
-			inputs  => inputs,
-			outputs => outputs
-		)
+		generic map(inputs  => inputs,	outputs => outputs)
 		port map(
 			clk      => clk,
 			rst      => rst,
@@ -53,23 +86,32 @@ begin
 			weight_i => weight_i,
 			output_o => output_o,
 			done_o   => done_o
-		);
+	);
+	
+	--Stimuli generation	
+    clk <= not clk after half_period;
+    start_stimuli : process is
+    begin
+        start_i <= '0', '1' after 15 ns, '0' after 30 ns;
+        wait until done_o = '1';
+    end process start_stimuli;
+    input_stimuli : process is
+    begin
+        input_i <= "10101";
+        weight_i <= ("11111","10101","01011","10111");
+        wait ;
+    end process;
 		
-		clk <= not clk after half_period;
-		
-		start_stimuli : process is
-		begin
-			start_i <= '0', '1' after 15 ns, '0' after 30 ns;
-			wait until done_o = '1';
-		end process start_stimuli;
-			
-		
-		input_stimuli : process is
-		begin
-			input_i <= "10101";
-			weight_i <= ("11111","10101","01011","10111");
-			wait ;
-		end process;
-		
-		
+	--Reading from the .csv file
+	file_open(my_csv_file, "/test_file_path", READ_MODE);
+	csv: process (rst) is
+	   begin
+        while not endfile(my_csv_file) loop
+            readline(my_csv_file, current_line);    -- Read line from file
+            for index in 0 to 10 loop   --THE RANGE NEEDS TO BE MODIFIED: loop for the numbers of elements in oen row
+                weight_i(index) <= std_logic_vector(TO_UNSIGNED(read_integer,1));   -- Read value from line
+            end loop;
+        end loop;
+   end process;
+    
 end behavioural;
